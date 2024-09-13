d82c260a80c140b133c6142ea28bc477f4573648
Karan030797 <karangowsick@gmail.com>
2024-09-13 17:17:06 +0530

    Update backend.tf

--- Jenkins-Server-TF/backend.tf
+++ Jenkins-Server-TF/backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket         = "my-ews-karan-1"
     region         = "us-east-1"
     key            = "End-to-End-Kubernetes-Three-Tier-DevSecOps-Project/Jenkins-Server-TF/terraform.tfstate"
-    dynamodb_table = "Lock-Files"
+    dynamodb_table = "Lock-Filess"
     encrypt        = true
   }
   required_version = ">=0.13.0"
